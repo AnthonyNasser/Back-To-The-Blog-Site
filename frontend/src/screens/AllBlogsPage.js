ba818f1599c0f073e2ef7db87b31c8039800a86e
AnthonyNasser <nasser.anthonyg@gmail.com>
2021-01-01 20:15:08 -0800

    Continued Redux implementation on AllBlogs page

--- frontend/src/screens/AllBlogsPage.js
+++ frontend/src/screens/AllBlogsPage.js
@@ -1,31 +1,32 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Blog from '../components/Blog'
-import axios from 'axios'
+import { listBlogs } from '../actions/blogActions'
 
 const AllBlogsPage = () => {
-	const [blogs, setBlog] = useState([])
+	const dispatch = useDispatch()
+
+	const blogList = useSelector((state) => state.blogList)
+	const { loading, error, blogs } = blogList
 
 	useEffect(() => {
-		const fetchBlogs = async () => {
-			const { data } = await axios.get('/api/blogs')
-
-			setBlog(data)
-		}
-
-		fetchBlogs()
-	}, [])
+		dispatch(listBlogs())
+	}, [dispatch])
 
 	return (
 		<>
 			<h1>Recently Posted Blogs</h1>
-			<Row>
-				{blogs.map((blog) => (
-					<Col key={blog._id} sm={12} md={6} lg={4} xl={3}>
-						<Blog blog={blog} />
-					</Col>
-				))}
-			</Row>
+			{ loading ? (<h2>Loading...</h2>) : error ? (<h3>{error}</h3>) : 
+			(
+				<Row>
+					{blogs.map((blog) => (
+						<Col key={blog._id} sm={12} md={6} lg={4} xl={3}>
+							<Blog blog={blog} />
+						</Col>
+					))}
+				</Row>
+			)}
 		</>
 	)
 }
