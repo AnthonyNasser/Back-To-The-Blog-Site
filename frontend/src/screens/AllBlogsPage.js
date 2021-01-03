8e554c6c95455a7570f435f421bbee98e042a27f
AnthonyNasser <nasser.anthonyg@gmail.com>
2021-01-03 08:17:49 -0800

    Redux implemented for BlogPage and AllBlogs Page, added actions, constants, reducers, and store

--- frontend/src/screens/AllBlogsPage.js
+++ frontend/src/screens/AllBlogsPage.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Blog from '../components/Blog'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import { listBlogs } from '../actions/blogActions'
 
 const AllBlogsPage = () => {
@@ -17,8 +19,11 @@ const AllBlogsPage = () => {
 	return (
 		<>
 			<h1>Recently Posted Blogs</h1>
-			{ loading ? (<h2>Loading...</h2>) : error ? (<h3>{error}</h3>) : 
-			(
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message variant='danger'>{error}</Message>
+			) : (
 				<Row>
 					{blogs.map((blog) => (
 						<Col key={blog._id} sm={12} md={6} lg={4} xl={3}>
