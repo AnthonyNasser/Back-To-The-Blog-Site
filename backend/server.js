f11806132e545e1ed298050b92e7081915c886a1
AnthonyNasser <nasser.anthonyg@gmail.com>
2021-03-29 23:38:29 -0700

    set up heroku

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,33 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
+import path from 'path'
 
 import blogRoutes from './routes/blogRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 connectDB()
+const __dirname = path.resolve()
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-	res.send('API is running...')
-})
-
 app.use('/api/blogs', blogRoutes)
 app.use('/api/users', userRoutes)
+
+if (process.env.NODE_ENV == 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+	)
+} else {
+	app.get('/', (req, res) => {
+		res.send('API is running...')
+	})
+}
 
 // 404 error handling
 app.use(notFound)
@@ -29,7 +39,5 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 app.listen(
 	PORT,
-	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-	)
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 )
